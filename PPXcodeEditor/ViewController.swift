7cf68a99aaec782a184740d459dc336386400011
ex_liuzp9 <ex_liuzp9@partner.midea.com>
2025-09-09 09:41:07 +0800

    Initial Commit

---
+++ PPXcodeEditor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PPXcodeEditor
+//
+//  Created by ex_liuzp9 on 2025/9/9.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
